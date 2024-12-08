7fba63558bd0d9c0dc307f051e0858180a84058c
viethoang003 <nguyenviethoanga2k3@gmail.com>
2024-12-08 14:03:55 +0700

    studentdemo

---
+++ src/studentdemo/StudentDemo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentdemo;
+
+/**
+ *
+ * @author Hoàng
+ */
+public class StudentDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFStudent jf = new JFStudent();
+        jf.show();
+        
+    }
+    
+}
---
+++ src/studentdemo/student.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentdemo;
+
+public class student {
+    private String ID;
+    private String name;
+    private int age;
+
+    public student(String ID, String name, int age) {
+        this.ID = ID;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    
+    
+    
+    
+    
+}
